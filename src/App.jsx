ba47d6458ecaa61c025e1a4946e5a6bc83c03576
dwarcho <dwarcho92@gmail.com>
2026-02-05 16:40:58 +0900

    부트스트랩 설치 및 쇼핑몰 기본 틀 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+        <Navbar bg="light" data-bs-theme="light">
+            <Container>
+                <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Cart</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+        <div className="main-bg"></div>
+
+        <div className="container mt-4">
+            <div className="row">
+                <div className="col-md-4">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt=""/>
+                    <h4>상품명</h4>
+                    <p>상품 설명</p>
+                </div>
+                <div className="col-md-4">
+                    <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt=""/>
+                    <h4>상품명</h4>
+                    <p>상품 설명</p>
+                </div>
+                <div className="col-md-4">
+                    <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt=""/>
+                    <h4>상품명</h4>
+                    <p>상품 설명</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
